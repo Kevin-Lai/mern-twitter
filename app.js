adb666d48f9d4bf053b7238d52516b9a5f89a8b4
Kevin-Lai <kevinlai249@gmail.com>
2020-10-25 15:23:33 -0700

    2nd commit

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello AA"));
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+const User = require("./models/User");
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect(db, { useNewUrlParser: true } )
+    .then(() => console.log("connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    });
+
+    user.save()
+
+    res.send("Hello AA");
+});
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
