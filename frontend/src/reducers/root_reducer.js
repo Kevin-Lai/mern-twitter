81ae66b1fe5e368581b35dcfb260d114922d1ab4
Kevin-Lai <kevinlai249@gmail.com>
2020-10-25 22:39:35 -0700

    Finished Tweets Feature

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,11 +3,16 @@ import session from './session_reducer';
 
 import errors from "./errors_reducer";
 import sessionErrors from "./session_errors_reducer";
+import tweets from "./tweets_reducer";
+
+import sessionAPI from "./session_api_reducer";
 
 const RootReducer = combineReducers({
   session,
   errors,
-  sessionErrors
+  sessionErrors,
+  sessionAPI,
+  tweets
 });
 
 export default RootReducer;
